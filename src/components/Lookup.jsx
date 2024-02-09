f15b24a84940213572cc640a9b671e0779d653cc
Faith Collison <faithcollison@yahoo.co.uk>
2024-02-09 17:21:25 +0000

    add loading bar from material ui

--- src/components/Lookup.jsx
+++ src/components/Lookup.jsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
+import LinearProgress from "@mui/material/LinearProgress";
 
-import { VehicleDetails } from './index'
+import { VehicleDetails } from "./index";
 import { getVehicleDetails } from "../api";
-import { StyledInput } from "./Lookup.styles"
-
+import { StyledInput } from "./Lookup.styles";
 
 export const Lookup = () => {
   const [searchInput, setSearchInput] = useState("");
   const [vehicleDetails, setVehicleDetails] = useState();
-  const [isLoading, setIsLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   function handleChange(event) {
     setSearchInput(event.target.value);
@@ -20,58 +20,57 @@ export const Lookup = () => {
 
   function handleSubmit(event) {
     event.preventDefault();
-    setIsLoading(true)
+    setIsLoading(true);
     getVehicleDetails(searchInput)
-    .then((data) => {
-      setVehicleDetails(data);
-      setSearchInput("");
-      setIsLoading(false)
-    })
-    .catch((err) => {
-      console.log(err)
-      setIsLoading(false)
-      setIsError(true)
-    });
+      .then((data) => {
+        setVehicleDetails(data);
+        setSearchInput("");
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        setIsError(true);
+      });
   }
 
-  if(isLoading) {
-    return <h2> Loading... </h2>
-  }
-
-  if(isError) {
+  if (isError) {
     return (
       <div>
         <p> Car not found </p>
         <p> Please try again </p>
       </div>
-    )
+    );
   }
 
   return (
     <div>
-      <form className="searchbar">
-        <label htmlFor="searchbar">
-          <StyledInput
-            type="text"
-            id="searchbar"
-            placeholder="Enter vehicle registration plate here..."
-            onChange={handleChange}
-            value={searchInput}
-          />
-        </label>
-        <Button
-          size="small"
-          variant="contained"
-          color="secondary"
-          onClick={handleSubmit}
-        >
-          <SearchIcon fontSize="large" color="action" />
-        </Button>
-      </form>
-      { vehicleDetails? (
+      {isLoading ? (
+        <LinearProgress />
+      ) : (
+        <form className="searchbar">
+          <label htmlFor="searchbar">
+            <StyledInput
+              type="text"
+              id="searchbar"
+              placeholder="Enter vehicle registration plate here..."
+              onChange={handleChange}
+              value={searchInput}
+            />
+          </label>
+          <Button
+            size="small"
+            variant="contained"
+            color="secondary"
+            onClick={handleSubmit}
+          >
+            <SearchIcon fontSize="large" color="action" />
+          </Button>
+        </form>
+      )}
+      {vehicleDetails ? (
         <VehicleDetails vehicleDetails={vehicleDetails} />
       ) : null}
     </div>
   );
 };
-
