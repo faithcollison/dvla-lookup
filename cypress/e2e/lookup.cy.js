2d90ecc3d6475bb5bf72477c10ab217e0d365f3f
Faith Collison <faithcollison@yahoo.co.uk>
2024-02-19 14:31:14 +0000

    merge changes from stash

--- cypress/e2e/lookup.cy.js
+++ cypress/e2e/lookup.cy.js
@@ -2,8 +2,12 @@ describe('My First Test', () => {
   it('Visits homepage', () => {
     cy.visit('https://faithcollison.github.io/dvla-lookup/')
     cy.findByLabelText("Reg Plate").type("MC20FLY{enter}");
-    cy.findByTestId("vehicle-details").within(() => {
-      cy.findByText("Registration Plate:").closest("li").findByText("MC20 FLY");
+    // cy.findByTestId("vehicle-details").within(() => {
+    //   cy.findByText("Registration Plate:").closest("li").findByText("MC20 FLY");
+    cy.get('[data-testid="vehicle-details"]').within(() => {
+      // Replace 'Registration Plate:' and 'MC20 FLY' with the actual text you expect
+      cy.contains('Registration Plate:').should('be.visible');
+      cy.contains('MC20 FLY').should('be.visible');
     });
   })
 })
