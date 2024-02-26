39d939e900c17d8cec41162b07a77eb9d3a7fde4
Faith Collison <faithcollison@yahoo.co.uk>
2024-02-26 12:05:38 +0000

    remove previous hosted link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://dvla-lookup.onrender.com"
-// axios.defaults.baseURL = "https://jen123ryri.execute-api.us-east-1.amazonaws.com/api/lookup?registrationNumber=MC20FLY"
 
 export async function getVehicleDetails(registrationNumber) {
       const response = await axios.get('/api/lookup', {
